42b0c47e01f14b3d62192e416668d93fc1d4a482
Peter Lang <lang.peter.github@gmail.com>
2023-12-05 16:40:27 +0100

    5

--- 5.py
+++ 5.py
@@ -46,41 +46,44 @@ class IndexedRange(Sequence):
 
 
 def map_seed_range_to_location(src_ranges):
-    def find_first_mapped_range(_m, s, max_len):
-        p = bisect_right(_m, (s + 1,))
-        # find last index which would have the same mapping
-        p_len = bisect_right(IndexedRange(s, max_len), p, key=lambda x: bisect_right(_m, (x + 1,)))
-        return p, p_len
-
     for m in mappings:
         dst_ranges = []
         for range_start, remaining_range_len in src_ranges:
             while remaining_range_len > 0:
-                # cut src_ranges by map_ranges starts:
-                # src_ranges: |-----|  |----------|   |---|
-                # map_ranges:     |-------|     |---|
-                # cut ranges: |---|-|  |--------|-|   |---|
-                # select first, along with the last mapped_range, that starts at the same point or before
-                idx, range_len = find_first_mapped_range(m, range_start, remaining_range_len)
+                # src_range:      |-----------|
+                # map_range: |idx-1....|   |idx......|
+                idx = bisect_right(m, (range_start + 1,))
+                if idx < len(m):
+                    # src_range:      |-----------|
+                    # map_range: |idx-1....|   |idx......|
+                    # select   :      |========|--|
+                    m_src_next, _, _ = m[idx]
+                    range_len = min(remaining_range_len, m_src_next - range_start)
+                else:
+                    # src_range:      |-----------|
+                    # map_range: |idx(-1)..|
+                    # select   :      |===========|
+                    range_len = remaining_range_len
+
                 if idx == 0:
-                    # src_range is before all existing mapping
+                    # src_range:    |--------|
+                    # map_range:             |idx(0)...|
+                    # select   :    |========|
                     dst_ranges.append((range_start, range_len))
                 else:
                     m_src, m_len, m_dst = m[idx - 1]
-                    if range_start >= m_src + m_len:
-                        # src_range:         |---|
-                        # map_range: |----|
-                        dst_ranges.append((range_start, range_len))
-                    else:
-                        # src_range: |-----.....|
-                        # map_range: |-----|
-                        mapped_len = min(m_src - range_start + m_len, range_len)
-                        unmapped_len = range_len - mapped_len
+                    mapped_len = max(0, min(m_src - range_start + m_len, range_len))
+                    unmapped_len = range_len - mapped_len
+                    if mapped_len:
+                        # src_range:      |--------|
+                        # map_range: |idx-1....|
+                        # select   :      |====|...|
                         dst_ranges.append((range_start - m_src + m_dst, mapped_len))
-                        if unmapped_len:
-                            # src_range: |.....-----|
-                            # map_range: |-----|
-                            dst_ranges.append((range_start + mapped_len, unmapped_len))
+                    if unmapped_len:
+                        # src_range:      |--------|
+                        # map_range: |idx-1....|
+                        # select   :      |....|===|
+                        dst_ranges.append((range_start + mapped_len, unmapped_len))
                 range_start += range_len
                 remaining_range_len -= range_len
         src_ranges = dst_ranges
