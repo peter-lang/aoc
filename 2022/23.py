41f93f67d8b1098f4b17221aa0f1c5208ebabbac
Peter Lang <lang.peter.github@gmail.com>
2023-12-18 17:38:50 +0100

    2022 - 23

--- 2022/23.py
+++ 2022/23.py
@@ -13,28 +13,28 @@ BOARD = np.array(
 )
 
 
-def round(b: np.array, offset: int):
+def execute_round(board: np.array, offset: int):
     padding = (
-        (1 if np.any(b[0, :] == 1) else 0, 1 if np.any(b[-1, :] == 1) else 0),
-        (1 if np.any(b[:, 0] == 1) else 0, 1 if np.any(b[:, -1] == 1) else 0),
+        (1 if np.any(board[0, :] == 1) else 0, 1 if np.any(board[-1, :] == 1) else 0),
+        (1 if np.any(board[:, 0] == 1) else 0, 1 if np.any(board[:, -1] == 1) else 0),
     )
     if padding != ((0, 0), (0, 0)):
-        b = np.pad(b, padding)
+        board = np.pad(board, padding)
 
-    r_max = b.shape[0]
-    c_max = b.shape[1]
+    r_max = board.shape[0]
+    c_max = board.shape[1]
 
-    no_east = np.zeros_like(b)
-    no_east[1:-1, 1:-1] = ~(b[:-2, 2:] | b[1:-1, 2:] | b[2:, 2:])
-    no_west = np.zeros_like(b)
-    no_west[1:-1, 1:-1] = ~(b[:-2, :-2] | b[1:-1, :-2] | b[2:, :-2])
-    no_north = np.zeros_like(b)
-    no_north[1:-1, 1:-1] = ~(b[:-2, :-2] | b[:-2, 1:-1] | b[:-2, 2:])
-    no_south = np.zeros_like(b)
-    no_south[1:-1, 1:-1] = ~(b[2:, :-2] | b[2:, 1:-1] | b[2:, 2:])
+    no_east = np.zeros_like(board)
+    no_east[1:-1, 1:-1] = ~(board[:-2, 2:] | board[1:-1, 2:] | board[2:, 2:])
+    no_west = np.zeros_like(board)
+    no_west[1:-1, 1:-1] = ~(board[:-2, :-2] | board[1:-1, :-2] | board[2:, :-2])
+    no_north = np.zeros_like(board)
+    no_north[1:-1, 1:-1] = ~(board[:-2, :-2] | board[:-2, 1:-1] | board[:-2, 2:])
+    no_south = np.zeros_like(board)
+    no_south[1:-1, 1:-1] = ~(board[2:, :-2] | board[2:, 1:-1] | board[2:, 2:])
 
-    no_move = b & no_south & no_west & no_east & no_north
-    rest = b & ~no_move
+    no_move = board & no_south & no_west & no_east & no_north
+    rest = board & ~no_move
     move_checks = [
         (no_north, (-1, 0)),
         (no_south, (1, 0)),
@@ -45,7 +45,7 @@ def round(b: np.array, offset: int):
     might_move = []
     for idx in range(4):
         no_neighbour, (r_d, c_d) = move_checks[(idx + offset) % 4]
-        move = np.zeros_like(b)
+        move = np.zeros_like(board)
         movable = rest & no_neighbour
 
         # move forward
@@ -86,7 +86,7 @@ def round(b: np.array, offset: int):
 # part 1
 b = np.copy(BOARD)
 for i in range(10):
-    b, _ = round(b, i)
+    b, _ = execute_round(b, i)
 edges = np.where(b)
 min_rect = (edges[0].max() - edges[0].min() + 1) * (edges[1].max() - edges[1].min() + 1)
 print(min_rect - np.sum(BOARD))
@@ -95,7 +95,7 @@ print(min_rect - np.sum(BOARD))
 i = 0
 b_prev = np.copy(BOARD)
 while True:
-    b, moved = round(b_prev, i)
+    b, moved = execute_round(b_prev, i)
     if not moved:
         break
     i += 1
