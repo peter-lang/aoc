2bcb9d73bc14beead78f4ae150a5bc639b27a02e
Peter Lang <lang.peter.github@gmail.com>
2023-12-19 21:35:12 +0100

    2021 7 revised

--- 2021/7.py
+++ 2021/7.py
@@ -20,6 +20,17 @@ def cost(med):
 # Function: SUM abs(a - i)**2_ / 2 (2_ means falling power of 2)
 # Discrete derivative: SUM abs(a - i)**2_ / 2 = -SUM (a - i) for i<a + SUM (i-a) for i>=a = SUM (i-a)
 # Maximum, where discrete derivative is 0 => i = sum(a) / len(a)
-# Note: this is an approximation, as chain-rule does not work seamlessly, but the error is negligible
+# Note: this is an approximation, as chain-rule is different in discrete calculus
+# we need to add an error term to the second derivative each time a sign change happens
+# in the abs function. This means a total of len(a) error in the first derivative, so
+# (sum(a)-len(a)) / len(a) <= i <= (sum(a)+len(a)) / len(a)
+
 squared_median = sum(numbers) / len(numbers)
-print(min(cost(math.ceil(squared_median)), cost(math.floor(squared_median))))
+print(
+    min(
+        cost(math.ceil(squared_median) - 1),
+        cost(math.ceil(squared_median)),
+        cost(math.floor(squared_median)),
+        cost(math.floor(squared_median) + 1),
+    )
+)
