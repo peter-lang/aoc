afcea3639d65578b7272b3ab998897c6d05d64e2
Peter Lang <lang.peter.github@gmail.com>
2023-12-30 22:57:06 +0100

    2020: 15

--- 2020/15.py
+++ 2020/15.py
@@ -2,22 +2,20 @@ NUMS = [0, 3, 1, 6, 7, 5]
 
 
 def spoken_number(nums, idx):
-    mem = {v: [idx] for idx, v in enumerate(nums)}
+    # we don't need the last value, as it would be read as first prev value during for-loop
+    mem = {v: idx for idx, v in enumerate(nums[:-1])}
 
-    turn = len(nums)
     prev = nums[-1]
-    while turn < idx:
-        if prev not in mem or len(mem[prev]) == 1:
+    for turn in range(len(nums), idx):
+        if prev not in mem:
             n = 0
         else:
-            history = mem[prev]
-            n = history[1] - history[0]
-        if n not in mem:
-            mem[n] = [turn]
-        else:
-            mem[n] = (mem[n] + [turn])[-2:]
+            # we know that prev was last said at (turn-1), mem[prev] stores the second to last turn
+            n = turn - 1 - mem[prev]
+        # update mem[prev] to its last turn, AFTER it has been read
+        # so during next read (after it has been spoken), it will contain second to last turn
+        mem[prev] = turn - 1
         prev = n
-        turn += 1
     return prev
 
 
